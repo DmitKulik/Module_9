3ad5d84b06eda00eecbf35495774937aa70e446f
Dmitry <grammattika@gmail.com>
2023-05-17 21:25:57 +0300

    Задание 9.1.3 Задание 9.1.4

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace Module_9
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 9.1.3
+            //Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+
+
+            //Задание 9.1.4
+            // Создайте класс исключения Exception и переопределите его свойство Message, а также свойство HelpLink, добавив в него ссылку на внешний ресурс.
+
+            Exception exception2 = new Exception();
+            exception2.HelpLink = "mail.ru";
+
+            
+
+        }
+    }
+}
+
