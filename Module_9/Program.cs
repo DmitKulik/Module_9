6eb07be20dd7a6d9cee3945f063bd84b9730e69d
Dmitry <grammattika@gmail.com>
2023-05-17 22:26:28 +0300

    тест кодировки

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,26 +1,30 @@
 ﻿
-using System;
 
 namespace Module_9
 
 {
     class Program
     {
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
         static void Main(string[] args)
         {
-            //Задание 9.1.3
-            //Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения
-            Exception exception = new Exception();
-            exception.Data.Add("Дата создания исключения : ", DateTime.Now);
-
-
-            //Задание 9.1.4
-            // Создайте класс исключения Exception и переопределите его свойство Message, а также свойство HelpLink, добавив в него ссылку на внешний ресурс.
-
-            Exception exception2 = new Exception();
-            exception2.HelpLink = "mail.ru";
-
+            try
+            {
+                int result = Division(7, 0);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                if (ex is DivideByZeroException) 
+                Console.WriteLine("Делить на нoль нельзя");
+            }
             
+
+            Console.ReadKey();
+
 
         }
     }
