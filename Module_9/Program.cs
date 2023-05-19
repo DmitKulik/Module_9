38a525f4d129729ac4a4e252116d692d724f4041
Dmitry <grammattika@gmail.com>
2023-05-19 20:40:38 +0300

    коменты к делигату

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,29 +1,32 @@
 ﻿
-
 using System.Net.Security;
 using System.Runtime.InteropServices;
 
-namespace Module_9
+// Определяем делегат CalculateDelegate, который принимает два целочисленных аргумента и возвращает целочисленное значение.
+delegate int CalculateDelegate(int a, int b);
+
+
+class Program
 {
-    delegate int CalculateDelegate(int a, int b);
-
-    class Program
+    
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        // Создаем экземпляр делегата CalculateDelegate, связывая его с методом Calculate.
+        CalculateDelegate calcDelegate = Calculate;
 
-            {
-                CalculateDelegate calcDelegate = Calculate;
-                int result = calcDelegate.Invoke(100, 30);
+        // Вызываем метод Calculate через делегат calcDelegate и передаем ему два целочисленных аргумента.
+        int result = calcDelegate.Invoke(100, 30);
 
-                Console.WriteLine(result);
-                Console.Read();
-            }
+       
+        Console.WriteLine(result);
 
-            static int Calculate(int a, int b)
-            {
-                return a - b;
-            }
-        }
+        
+        Console.Read();
+    }
+
+    // Определяем статический метод Calculate, который принимает два целочисленных аргумента и возвращает разность первого и второго аргументов.
+    static int Calculate(int a, int b)
+    {
+        return a - b;
     }
 }
