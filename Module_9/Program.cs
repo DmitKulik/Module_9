839287b0229632ad605fa20f18ad329814557570
Dmitry <grammattika@gmail.com>
2023-05-20 00:30:03 +0300

    многоадресный делегат

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -3,33 +3,28 @@ using System.Net.Security;
 using System.Runtime.InteropServices;
 
 // Определяем делегат CalculateDelegate, который принимает два целочисленных аргумента и возвращает целочисленное значение.
-delegate int CalculateDelegate(int a, int b);
-
+ 
 
 class Program
 {
-    
+    public delegate void TestDelegate(int a, int b);
+
     static void Main(string[] args)
     {
-        // 1 способ вызова делигата. Создаем экземпляр делегата CalculateDelegate, связывая его с методом Calculate.
-        CalculateDelegate calcDelegate = Calculate;
-        int result = calcDelegate.Invoke(100, 30);
-        Console.WriteLine(result);
-        Console.Read();
+       /* TestDelegate testDelegate = CalculateSubtraction;
+        testDelegate += CalculateAddition;
+        testDelegate.Invoke(100, 200);
 
-
-        // 2 способ вызова делигата. Создаем экземпляр делегата CalculateDelegate, связывая его с методом Calculate.
-        CalculateDelegate calcDelegate2 = Calculate;
-        int result2 = calcDelegate(100, 30); // разница в методе .Invoke
-        Console.WriteLine(result);
-        Console.Read();
-
-
+        Console.Read();*/
     }
 
-    // Определяем статический метод Calculate, который принимает два целочисленных аргумента и возвращает разность первого и второго аргументов.
-    static int Calculate(int a, int b)
+    static void CalculateSubtraction(int a, int b)
     {
-        return a - b;
+        Console.WriteLine(a - b);
+    }
+
+    static void CalculateAddition(int a, int b)
+    {
+        Console.WriteLine(a + b);
     }
 }
