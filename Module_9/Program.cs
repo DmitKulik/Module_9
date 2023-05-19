e4051fe31bfc566a365fefb0d88f180496748952
Dmitry <grammattika@gmail.com>
2023-05-20 00:48:59 +0300

    приминение шаблонного делегата

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -2,29 +2,55 @@
 using System.Net.Security;
 using System.Runtime.InteropServices;
 
-// Определяем делегат CalculateDelegate, который принимает два целочисленных аргумента и возвращает целочисленное значение.
- 
 
-class Program
+public class Program
 {
-    public delegate void TestDelegate(int a, int b);
+    // Определяем делегат, который не принимает аргументы и не возвращает значения
+    delegate void ShowMessageDelegate();
+    // Определяем делегат, который принимает три аргумента типа int и возвращает значение типа int
+    delegate int SumDelegate(int a, int b, int c);
+    // Определяем делегат, который принимает один аргумент типа string и возвращает значение типа bool
+    delegate bool CheckLengthDelegate(string row);
 
     static void Main(string[] args)
     {
-       /* TestDelegate testDelegate = CalculateSubtraction;
-        testDelegate += CalculateAddition;
-        testDelegate.Invoke(100, 200);
+        // Создаем экземпляр делегата ShowMessageDelegate и присваиваем ему метод ShowMessage
+        Action showMessageDelegate = ShowMessage;
+        // Вызываем метод Invoke у делегата для выполнения метода ShowMessage
+        showMessageDelegate.Invoke();
 
-        Console.Read();*/
+        // Создаем экземпляр делегата SumDelegate и присваиваем ему метод Sum
+        Func<int, int, int, int> sumDelegate = Sum;
+        // Вызываем метод Invoke у делегата для выполнения метода Sum с аргументами 1, 30, 120
+        int result = sumDelegate.Invoke(1, 30, 120);
+        Console.WriteLine(result);
+
+        // Создаем экземпляр делегата CheckLengthDelegate и присваиваем ему метод CheckLength
+        Predicate<string> checkLengthDelegate = CheckLength;
+        // Вызываем метод Invoke у делегата для выполнения метода CheckLength с аргументом "skill_factory"
+        bool status = checkLengthDelegate.Invoke("skill_factory");
+        Console.WriteLine(status);
+
+        Console.ReadLine();
     }
 
-    static void CalculateSubtraction(int a, int b)
+    // Определяем статический метод ShowMessage, который выводит на консоль строку "Hello World!"
+    static void ShowMessage()
     {
-        Console.WriteLine(a - b);
+        Console.WriteLine("Hello World!");
     }
 
-    static void CalculateAddition(int a, int b)
+    // Определяем статический метод Sum, который принимает три аргумента типа int и возвращает их сумму
+    static int Sum(int a, int b, int c)
     {
-        Console.WriteLine(a + b);
+        return a + b + c;
+    }
+
+    // Определяем статический метод CheckLength, который принимает один аргумент типа string и возвращает значение типа bool в зависимости от длины аргумента
+    static bool CheckLength(string _row)
+    {
+        if (_row.Length > 3)
+            return true;
+        return false;
     }
 }
